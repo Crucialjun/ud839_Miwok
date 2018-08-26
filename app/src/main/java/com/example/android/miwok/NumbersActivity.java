4364c46be268a0869808865b7c8c005d9234f6f2
Crucialjun <nich.otieno@gmail.com>
2018-08-27 00:11:53 +0300

    ArrayLists Additions

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +27,24 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Initialize the string array
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+
+
+        Log.v("NumbersActivity","Word at index 0: " + words.get(0));
+        Log.v("NumbersActivity","Word at index 0: " + words.get(1));
+        Log.v("NumbersActivity","Word at index 0: " + words.get(2));
     }
 }
