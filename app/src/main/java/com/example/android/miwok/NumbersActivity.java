87264315fcd237dba985131124bb168bf5a2cbeb
Crucialjun <nich.otieno@gmail.com>
2018-08-29 21:58:11 +0300

    Listview addition

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,7 +18,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -48,14 +50,12 @@ public class NumbersActivity extends AppCompatActivity {
 
 
 
-        LinearLayout rootView = findViewById(R.id.rootView);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>
+                (this,android.R.layout.simple_list_item_1);
 
-        for ( int startPoint = 0; startPoint < words.size() ;startPoint++) {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(startPoint));
-            rootView.addView(wordView);
-        }
+        ListView listView = (ListView) findViewById(R.id.list);
 
+        listView.setAdapter(itemsAdapter);
 
 
     }
