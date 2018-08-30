2b9138535f1ffff266fd24566cf8d9113a9e1cb2
Crucialjun <nich.otieno@gmail.com>
2018-08-31 00:12:26 +0300

    Array Adapater update

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -17,6 +17,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -24,5 +27,24 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        //Initialize the string array
+        ArrayList<Word> words = new ArrayList<>();
+
+        words.add(new Word ("Red","Weteti"));
+        words.add(new Word ("Green","Chokokki"));
+        words.add(new Word ("Brown","Takaakki"));
+        words.add(new Word ("Gray","Topoppi"));
+        words.add(new Word ("Black","Kululli"));
+        words.add(new Word ("White","Kelelli"));
+        words.add(new Word ("Dusty yellow","ṭopiisә"));
+        words.add(new Word ("mustard yellow","chiwiiṭә"));
+
+
+        WordAdapter adapter = new WordAdapter(this,words);
+
+        ListView listView = findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
     }
 }
